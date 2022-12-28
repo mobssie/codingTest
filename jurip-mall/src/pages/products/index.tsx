bb6436072b24d5fdc46cd6b830a58f3cef77d273
mobssie <mobssie.jung@gmail.com>
2022-12-29 02:10:52 +0900

    fetch

--- jurip-mall/src/pages/products/index.tsx
+++ jurip-mall/src/pages/products/index.tsx
@@ -1,3 +1,15 @@
-const ProductList = ()=> <div>List</div>
+import { useQuery } from 'react-query'
+import { fetcher, QueryKeys } from '../../queryClient'
+const ProductList = ()=> {
+  const { data } = useQuery(QueryKeys.PRODUCTS, ()=> fetcher({
+    method: 'GET',
+    path: '/products'
+  }))
+  console.log('data', data)
+
+
+  
+  return (<div>List</div>)
+}
 
 export default ProductList
