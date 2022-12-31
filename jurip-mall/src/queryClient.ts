b20bf96d8e4d49b3070c835a529776abb7151b88
mobssie <mobssie.jung@gmail.com>
2022-12-31 15:40:16 +0900

    reactQeury option 캐싱 처리

--- jurip-mall/src/queryClient.ts
+++ jurip-mall/src/queryClient.ts
@@ -12,7 +12,17 @@ type AnyOBJ = { [key: string]: any }
 export const getClient = (()=> {
   let client: QueryClient | null = null;
   return ()=> {
-    if(!client) client = new QueryClient({});
+    if(!client) client = new QueryClient({
+      defaultOptions: {
+        queries: {
+          cacheTime: 1000 * 60 * 60 * 60 * 24,
+          staleTime: 1000,
+          refetchOnMount: false,
+          refetchOnReconnect: false,
+          refetchOnWindowFocus: false,
+        }
+      }
+    });
     return client
   }
 })()
