2b5e99057b18d242ea5af5d07a8dcc49088615ad
mobssie <mobssie.jung@gmail.com>
2023-01-01 23:41:37 +0900

    fix graphql handler (ADD_CART, UPDATE_CART)

--- jurip-mall/src/mocks/handlers.ts
+++ jurip-mall/src/mocks/handlers.ts
@@ -29,35 +29,28 @@ export const handlers = [
     return res(ctx.data(cartData))
   }),
   graphql.mutation(ADD_CART, (req, res, ctx)=> {
-    const newData = {...cartData}
+    const newCartData = {...cartData}
     const id = req.variables.id
-    if (newData[id]) {
-      newData[id] = {
-        ...newData[id],
-        amount: (newData[id].amount || 0) + 1,
-      }
-    } else {
-      const found = mockProducts.find(item => item.id === req.variables.id)
-      if(found) {
-        newData[id] = {
-          ...found,
-          amount: 1
-        }
-      }
+    const targetProduct = mockProducts.find(item => item.id === req.variables.id)
+    if(!targetProduct) { throw new Error('상품이 없습니다.')}
+    const newItem = {
+        ...targetProduct,
+        amount: (newCartData[id]?.amount || 0) + 1,
     }
-    cartData = newData
-    console.log('cartData ::', cartData)
-    return res(ctx.data(newData))
+    newCartData[id] = newItem
+    cartData = newCartData
+    return res(ctx.data(newItem))
   }),
   graphql.mutation(UPDATE_CART,(req, res, ctx)=> {
     const newData = {...cartData}
     const { id, amount } = req.variables
     if(!newData[id]) { throw new Error('없는 데이터입니다.')}
-    newData[id] = {
+    const newItem = {
       ...newData[id],
       amount,
     }
+    newData[id] = newItem
     cartData = newData
-    return res(ctx.data(newData))
+    return res(ctx.data(newItem))
   }),
 ]
